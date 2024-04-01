4b52e8b9a32b299e9a30caf9d6ffe041eef0aa5b
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-04-01 20:10:09 +0530

    fin

--- script/registration-form.js
+++
@@ -1,9 +0,0 @@
-
-var checked_container = document.getElementById('checked');
-var form_container = document.getElementById('form-container');
-
-function displayCheck(){
-    
-        form_container.style.display="none";
-        checked_container.style.display="flex";
-}
