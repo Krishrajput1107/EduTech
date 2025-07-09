aab5ab0be6863af0192fe20d833ee127fd7de211
Krish <devgrade687@gmail.com>
2025-07-10 02:54:26 +0530

    Fixed options in quiz

--- script/nodejsquiz.js
+++ script/nodejsquiz.js
@@ -2,21 +2,21 @@ let questions = [
     {
         numb: 1,
         question: "What is Node.js primarily suitable for?",
-        answer: "C. Developing highly scalable back-end services (Correct Answer)",
+        answer: "C. Developing highly scalable back-end services",
         options: [
             "A. Building front-end interfaces",
             "B. Creating CPU-intensive applications",
-            "C. Developing highly scalable back-end services (Correct Answer)",
+            "C. Developing highly scalable back-end services",
             "D. Designing mobile applications"
         ]
     },
     {
         numb: 2,
         question: "In Node.js, what is used to handle all requests efficiently?",
-        answer: "B. Single thread (Correct Answer)",
+        answer: "B. Single thread",
         options: [
             "A. Multiple threads",
-            "B. Single thread (Correct Answer)",
+            "B. Single thread",
             "C. Forked processes",
             "D. Task queues"
         ]
@@ -24,7 +24,7 @@ let questions = [
     {
         numb: 3,
         question: "How does Node.js handle CPU-intensive applications?",
-        answer: "D. Not suitable for CPU-intensive applications (Correct Answer)",
+        answer: "D. Not suitable for CPU-intensive applications",
         options: [
             "A. Efficiently",
             "B. In parallel",
@@ -35,9 +35,9 @@ let questions = [
     {
         numb: 4,
         question: "What type of applications is Node.js ideal for building?",
-        answer: "A. Data-intensive and real-time applications (Correct Answer)",
+        answer: "A. Data-intensive and real-time applications",
         options: [
-            "A. Data-intensive and real-time applications (Correct Answer)",
+            "A. Data-intensive and real-time applications",
             "B. Offline applications",
             "C. Single-page applications",
             "D. Graphic-intensive applications"
@@ -46,43 +46,43 @@ let questions = [
     {
         numb: 5,
         question: "In Node.js, what replaces the window object?",
-        answer: "C. Global object (Correct Answer)",
+        answer: "C. Global object",
         options: [
             "A. Document object",
             "B. Navigator object",
-            "C. Global object (Correct Answer)",
+            "C. Global object",
             "D. Window variable"
         ]
     },
     {
         numb: 6,
         question: "What does every file in Node.js act as?",
-        answer: "C. Module (Correct Answer)",
+        answer: "C. Module",
         options: [
             "A. Class",
             "B. Function",
-            "C. Module (Correct Answer)",
+            "C. Module",
             "D. Object"
         ]
     },
     {
         numb: 7,
         question: "How can module functions be made public in Node.js?",
-        answer: "C. Exporting them (Correct Answer)",
+        answer: "C. Exporting them",
         options: [
             "A. Using constants",
             "B. Declaring globally",
-            "C. Exporting them (Correct Answer)",
+            "C. Exporting them",
             "D. Assigning to global variables"
         ]
     },
     {
         numb: 8,
         question: "What does the Node.js OS module provide?",
-        answer: "B. System information (Correct Answer)",
+        answer: "B. System information ",
         options: [
             "A. Access to external databases",
-            "B. System information (Correct Answer)",
+            "B. System information ",
             "C. File handling functions",
             "D. Network operations"
         ]
@@ -90,32 +90,32 @@ let questions = [
     {
         numb: 9,
         question: "How are asynchronous methods in Node.js typically handled?",
-        answer: "C. Using a callback function (Correct Answer)",
+        answer: "C. Using a callback function",
         options: [
             "A. Synchronously",
             "B. Using promises",
-            "C. Using a callback function (Correct Answer)",
+            "C. Using a callback function",
             "D. In parallel"
         ]
     },
     {
         numb: 10,
         question: "What is the core functionality of Node.js based on?",
-        answer: "C. Events (Correct Answer)",
+        answer: "C. Events ",
         options: [
             "A. Promises",
             "B. Callbacks",
-            "C. Events (Correct Answer)",
+            "C. Events ",
             "D. Asynchronous operations"
         ]
     },
     {
         numb: 11,
         question: "How can listener functions in Node.js receive data for an event?",
-        answer: "B. By accessing the event object (Correct Answer)",
+        answer: "B. By accessing the event object ",
         options: [
             "A. Through global variables",
-            "B. By accessing the event object (Correct Answer)",
+            "B. By accessing the event object ",
             "C. Via callback functions",
             "D. By invoking another function"
         ]
@@ -123,43 +123,43 @@ let questions = [
     {
         numb: 12,
         question: "How can a custom class with additional capabilities be created in Node.js?",
-        answer: "C. By inheriting from the Event Emitter (Correct Answer)",
+        answer: "C. By inheriting from the Event Emitter ",
         options: [
             "A. By extending the Node.js core",
             "B. By creating a new module",
-            "C. By inheriting from the Event Emitter (Correct Answer)",
+            "C. By inheriting from the Event Emitter ",
             "D. By declaring additional functions"
         ]
     },
     {
         numb: 13,
         question: "What kind of events does a Node.js server typically raise?",
-        answer: "D. New connection or request events (Correct Answer)",
+        answer: "D. New connection or request events ",
         options: [
             "A. Timer events",
             "B. Exception events",
             "C. I/O events",
-            "D. New connection or request events (Correct Answer)"
+            "D. New connection or request events "
         ]
     },
     {
         numb: 14,
         question: "What is used in Node.js to create a structured way of handling routes?",
-        answer: "C. Express framework (Correct Answer)",
+        answer: "C. Express framework",
         options: [
             "A. Custom modules",
             "B. Event Emitter",
-            "C. Express framework (Correct Answer)",
+            "C. Express framework",
             "D. OS module"
         ]
     },
     {
         numb: 15,
         question: "How does Node.js handle HTTP requests?",
-        answer: "B. Using a single thread (Correct Answer)",
+        answer: "B. Using a single thread",
         options: [
             "A. Using multiple threads",
-            "B. Using a single thread (Correct Answer)",
+            "B. Using a single thread",
             "C. Creating child processes",
             "D. Relying on external libraries"
         ]
@@ -167,42 +167,42 @@ let questions = [
     {
         numb: 16,
         question: "What coding paradigm does Node.js primarily follow?",
-        answer: "C. Asynchronous programming (Correct Answer)",
+        answer: "C. Asynchronous programming",
         options: [
             "A. Functional programming",
             "B. Object-oriented programming",
-            "C. Asynchronous programming (Correct Answer)",
+            "C. Asynchronous programming",
             "D. Sequential programming"
         ]
     },
     {
         numb: 17,
         question: "What is an important feature of Node.js that enables efficient event-driven programming?",
-        answer: "C. Event loop (Correct Answer)",
+        answer: "C. Event loop",
         options: [
             "A. Callback chaining",
             "B. Error handling",
-            "C. Event loop (Correct Answer)",
+            "C. Event loop",
             "D. Microservices"
         ]
     },
     {
         numb: 18,
         question: "How does Node.js handle scalability in applications?",
-        answer: "D. By utilizing non-blocking I/O operations (Correct Answer)",
+        answer: "D. By utilizing non-blocking I/O operations",
         options: [
             "A. By using multithreading",
             "B. By load balancing across multiple servers",
             "C. By optimizing CPU usage",
-            "D. By utilizing non-blocking I/O operations (Correct Answer)"
+            "D. By utilizing non-blocking I/O operations"
         ]
     },
     {
         numb: 19,
         question: "What role does the package.json file play in Node.js projects?",
-        answer: "A. It defines project dependencies and scripts (Correct Answer)",
+        answer: "A. It defines project dependencies and scripts",
         options: [
-            "A. It defines project dependencies and scripts (Correct Answer)",
+            "A. It defines project dependencies and scripts",
             "B. It stores user interface configurations",
             "C. It manages database connections",
             "D. It controls the server routing"
@@ -211,9 +211,9 @@ let questions = [
     {
         numb: 20,
         question: "What is the primary role of the 'package.json' file in Node.js projects?",
-        answer: "A. It defines project dependencies and scripts (Correct Answer)",
+        answer: "A. It defines project dependencies and scripts",
         options: [
-            "A. It defines project dependencies and scripts (Correct Answer)",
+            "A. It defines project dependencies and scripts",
             "B. It handles user authentication",
             "C. It manages server configurations",
             "D. It controls client-side routing"
--- script/sqlquiz.js
+++ script/sqlquiz.js
@@ -2,9 +2,9 @@ let questions = [
     {
         numb: 1,
         question: "What does SQL stand for?",
-        answer: "A) Structured Query Language (Correct Answer)",
+        answer: "A) Structured Query Language",
         options: [
-            "A) Structured Query Language (Correct Answer)",
+            "A) Structured Query Language",
             "B) Sequential Query Language",
             "C) Static Query Language",
             "D) Simple Query Language"
@@ -13,10 +13,10 @@ let questions = [
     {
         numb: 2,
         question: "Which SQL keyword is used to retrieve data from a database?",
-        answer: "B) SELECT (Correct Answer)",
+        answer: "B) SELECT",
         options: [
             "A) FIND",
-            "B) SELECT (Correct Answer)",
+            "B) SELECT" ,
             "C) GET",
             "D) FETCH"
         ]
@@ -24,31 +24,31 @@ let questions = [
     {
         numb: 3,
         question: "What is the function of the SQL keyword 'WHERE'?",
-        answer: "C) To filter rows based on a condition (Correct Answer)",
+        answer: "C) To filter rows based on a condition",
         options: [
             "A) To specify the columns to be retrieved",
             "B) To perform calculations on data",
-            "C) To filter rows based on a condition (Correct Answer)",
+            "C) To filter rows based on a condition" ,
             "D) To sort the result set"
         ]
     },
     {
         numb: 4,
         question: "Which SQL command is used to insert new data into a database table?",
-        answer: "C) INSERT (Correct Answer)",
+        answer: "C) INSERT",
         options: [
             "A) UPDATE",
             "B) ALTER",
-            "C) INSERT (Correct Answer)",
+            "C) INSERT" ,
             "D) ADD"
         ]
     },
     {
         numb: 5,
         question: "In SQL, which clause is used to sort the result set in ascending or descending order?",
-        answer: "A) ORDER BY (Correct Answer)",
+        answer: "A) ORDER BY",
         options: [
-            "A) ORDER BY (Correct Answer)",
+            "A) ORDER B ",
             "B) SORT BY",
             "C) GROUP BY",
             "D) ARRANGE BY"
@@ -57,10 +57,10 @@ let questions = [
     {
         numb: 6,
         question: "What is the SQL keyword used to delete records from a table?",
-        answer: "B) DELETE (Correct Answer)",
+        answer: "B) DELETE",
         options: [
             "A) REMOVE",
-            "B) DELETE (Correct Answer)",
+            "B) DELETE",
             "C) ERASE",
             "D) DISCARD"
         ]
@@ -68,10 +68,10 @@ let questions = [
     {
         numb: 7,
         question: "Which SQL statement is used to make changes to the structure of a table?",
-        answer: "B) ALTER TABLE (Correct Answer)",
+        answer: "B) ALTER TABLE",
         options: [
             "A) MODIFY",
-            "B) ALTER TABLE (Correct Answer)",
+            "B) ALTER TABLE",
             "C) CHANGE",
             "D) UPDATE TABLE"
         ]
@@ -79,21 +79,21 @@ let questions = [
     {
         numb: 8,
         question: "What is the purpose of the SQL keyword 'GROUP BY'?",
-        answer: "D) To group rows with identical values into summary rows (Correct Answer)",
+        answer: "D) To group rows with identical values into summary rows",
         options: [
             "A) To filter rows based on a condition",
             "B) To perform calculations on grouped data",
             "C) To sort the result set",
-            "D) To group rows with identical values into summary rows (Correct Answer)"
+            "D) To group rows with identical values into summary rows"
         ]
     },
     {
         numb: 9,
         question: "Which SQL function is used to count the number of rows in a result set?",
-        answer: "B) COUNT() (Correct Answer)",
+        answer: "B) COUNT()",
         options: [
             "A) TOTAL()",
-            "B) COUNT() (Correct Answer)",
+            "B) COUNT()",
             "C) SUM()",
             "D) CALCULATE()"
         ]
@@ -101,9 +101,9 @@ let questions = [
     {
         numb: 10,
         question: "What SQL command is used to retrieve all rows and columns from a table?",
-        answer: "A) SELECT * (Correct Answer)",
+        answer: "A) SELECT *",
         options: [
-            "A) SELECT * (Correct Answer)",
+            "A) SELECT *",
             "B) SELECT ALL",
             "C) SELECT ROWS",
             "D) FETCH ALL"
@@ -112,20 +112,20 @@ let questions = [
     {
         numb: 11,
         question: "Which SQL operator is used to compare two values for equality?",
-        answer: "D) != (Correct Answer)",
+        answer: "D) != ",
         options: [
             "A) =",
             "B) ==",
             "C) <>",
-            "D) != (Correct Answer)"
+            "D) != "
         ]
     },
     {
         numb: 12,
         question: "Which SQL clause is used to specify the conditions that must be met to retrieve rows from a table?",
-        answer: "A) WHERE (Correct Answer)",
+        answer: "A) WHERE",
         options: [
-            "A) WHERE (Correct Answer)",
+            "A) WHERE",
             "B) HAVING",
             "C) BY",
             "D) FROM"
@@ -134,31 +134,31 @@ let questions = [
     {
         numb: 13,
         question: "What is the purpose of the SQL keyword 'JOIN'?",
-        answer: "C) To combine rows from two or more tables based on a related column (Correct Answer)",
+        answer: "C) To combine rows from two or more tables based on a related column",
         options: [
             "A) To specify the columns to be retrieved",
             "B) To filter rows based on a condition",
-            "C) To combine rows from two or more tables based on a related column (Correct Answer)",
+            "C) To combine rows from two or more tables based on a related column",
             "D) To sort the result set"
         ]
     },
     {
         numb: 14,
         question: "Which SQL data type is used to store fixed-length character strings?",
-        answer: "C) CHAR (Correct Answer)",
+        answer: "C) CHAR",
         options: [
             "A) VARCHAR",
             "B) TEXT",
-            "C) CHAR (Correct Answer)",
+            "C) CHAR",
             "D) STRING"
         ]
     },
     {
         numb: 15,
         question: "What is the purpose of the SQL keyword 'DISTINCT'?",
-        answer: "A) To retrieve unique values from a column (Correct Answer)",
+        answer: "A) To retrieve unique values from a column",
         options: [
-            "A) To retrieve unique values from a column (Correct Answer)",
+            "A) To retrieve unique values from a column",
             "B) To specify the columns to be retrieved",
             "C) To filter rows based on a condition",
             "D) To sort the result set"
@@ -167,9 +167,9 @@ let questions = [
     {
         numb: 16,
         question: "Which SQL function is used to find the maximum value in a column?",
-        answer: "A) MAX() (Correct Answer)",
+        answer: "A) MAX()",
         options: [
-            "A) MAX() (Correct Answer)",
+            "A) MAX()",
             "B) MIN()",
             "C) AVG()",
             "D) SUM()"
@@ -178,9 +178,9 @@ let questions = [
     {
         numb: 17,
         question: "What SQL clause is used to limit the number of rows returned in a result set?",
-        answer: "A) LIMIT (Correct Answer)",
+        answer: "A) LIMIT",
         options: [
-            "A) LIMIT (Correct Answer)",
+            "A) LIMIT",
             "B) TOP",
             "C) ROWS",
             "D) FETCH"
@@ -189,9 +189,9 @@ let questions = [
     {
         numb: 18,
         question: "Which SQL statement is used to change the name of a table?",
-        answer: "A) RENAME TABLE (Correct Answer)",
+        answer: "A) RENAME TABLE",
         options: [
-            "A) RENAME TABLE (Correct Answer)",
+            "A) RENAME TABLE",
             "B) CHANGE TABLE NAME",
             "C) ALTER TABLE NAME",
             "D) MODIFY TABLE"
@@ -200,23 +200,23 @@ let questions = [
     {
         numb: 19,
         question: "What is the purpose of the SQL keyword 'HAVING'?",
-        answer: "C) To filter rows in a grouped result set based on a condition (Correct Answer)",
+        answer: "C) To filter rows in a grouped result set based on a condition",
         options: [
             "A) To filter rows based on a condition",
             "B) To specify the columns to be retrieved",
-            "C) To filter rows in a grouped result set based on a condition (Correct Answer)",
+            "C) To filter rows in a grouped result set based on a condition",
             "D) To sort the result set"
         ]
     },
     {
         numb: 20,
         question: "Which SQL command is used to retrieve data from two or more tables based on a related column between them?",
-        answer: "D) JOIN (Correct Answer)",
+        answer: "D) JOIN",
         options: [
             "A) LINK",
             "B) UNION",
             "C) MERGE",
-            "D) JOIN (Correct Answer)"
+            "D) JOIN"
         ]
     }
 ];
